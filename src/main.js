00afa097f4332a1588ecf4c761d9f3db727c25d8
Verkunder <verkunder614@gmail.com>
2022-02-12 15:41:53 +0300

    add style and index banner

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
+import './assets/css/material-kit.min.css'
+import './assets/css/nucleo-icons.css'
+import "./assets/main.css"
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
+    <Banner />
     <h1>{{massage}}</h1>
   </div>
 </template>
 
 <script>
+import Banner from '../components/Home/Banner.vue'
   export default { 
     data() {
       return {
         massage: 'Hello World'
       }
-    }
-    
+    },
+    components: {Banner}    
   }
 </script>
 
